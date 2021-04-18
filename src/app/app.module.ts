e5292781a7a872c83a39d050718c04b29cb417b2
Arman Chand <armanchand45@gmail.com>
2021-04-18 19:13:41 +0530

    secod commit Assignment1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,10 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
